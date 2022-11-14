02fe44dfd67c30e69578c8fa47e38125eb5cc818
jason044 <jason044@gmail.com>
2022-11-14 09:42:45 -0800

    initial
    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BuildingEscapeTut
+
+Developed with Unreal Engine 5
---
+++ Source/BuildingEscapeTut/BuildingEscapeTutGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingEscapeTutGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPETUT_API ABuildingEscapeTutGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

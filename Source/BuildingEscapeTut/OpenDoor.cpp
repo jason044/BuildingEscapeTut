e0f23895c0e664fb66f5c05420ff95adead4bd53
jason044 <jason044@gmail.com>
2022-12-07 10:59:12 -0800

    added visibile anywhere to opendoor
    also flipped the door rotation negatice

--- Source/BuildingEscapeTut/OpenDoor.cpp
+++ Source/BuildingEscapeTut/OpenDoor.cpp
@@ -23,7 +23,7 @@ void UOpenDoor::BeginPlay()
 	AActor* Owner = GetOwner();
 	
 	//Create a rotator
-	FRotator NewRotation = FRotator(0.f, 60.f, 0.f);
+	FRotator NewRotation = FRotator(0.f, -60.f, 0.f);
 
 	//Set the door rotation
 	Owner->SetActorRotation(NewRotation);
